489679ed722b147b7895b36e4268f6445b1c9c8c
denosy <50819494+denosy@users.noreply.github.com>
2021-03-06 23:31:45 +0300

    share

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import HomeScreen from './screens/HomeScreen';
 import {BrowserRouter, Link, Route} from 'react-router-dom'
 import CartScreen from './screens/CartScreen';
 import { useSelector } from 'react-redux';
+import SigninScreen from './screens/SigninScreen';
 function App() {
 
   const cart = useSelector(state => state.cart);
@@ -30,6 +31,7 @@ function App() {
     <main>
       <Route path="/cart/:id?" component={CartScreen}></Route>
       <Route path="/room/:id" component={RoomScreen}></Route>
+      <Route path="/signin" component={SigninScreen}></Route>
       <Route path="/" component={HomeScreen} exact></Route>
       
     </main>
